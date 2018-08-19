d951130ba8820ae7391368aa3a9713dbc0409d90
Clara Bell <athenaolson@Athenas-MacBook-Pro.local>
2018-08-19 19:19:42 -0400

    working commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,23 @@
+// lines 2-18 are what generates the greeting on the browser depending on what time it is (less than 12:00 good morning, more that 12:00 good afternoon, more than 5:00 pm good evening. I wanted to be able to style this with a color or something, and also thought that it would be neat to make the greeting disappear after the user begins the game, but I couldn't figure it out.)
 var d = new Date();
     var time = d.getHours();
     var greeting = ""
 
     if (time < 12) 
       {
-      greeting = ("<h2>Good morning!</h2>");
+      greeting = ("<h2>Good Morning!</h2>");
       }
     if (time > 12) 
       {
-      greeting = ("<h2>Good afternoon!</h2>");
+      greeting = ("<h2>Good Afternoon!</h2>");
       }
-    if (time > 18) 
+    if (time > 17) 
       {
       greeting = ("<h2>Good Evening!</h2>");
       }
       document.write(greeting);
-		
+    
+    //   this is the code that makes my game work. lines 20-end.
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
 var wins = 0;
@@ -27,7 +29,7 @@ document.onkeyup = function(event) {
 
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     
-   //  var userGuess = event.key;
+   // this also works over what I have in line 29, but what's in line 29 tells the computer to change uppercase userstrokes to lowercase ----> var userGuess = event.key;
 
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
@@ -55,10 +57,10 @@ document.onkeyup = function(event) {
 
        
    }
-
+// notice line 63 where this script populates new text to appear on the browser. I thought that was a cool way to understand how this section works in the code, although I don't completely understand it.
    var html = 
    "<h1> Let's Play The Psychic Game! </h1>" +
-   "<p>You can't beat The Psychic!</p>" +
+   "<h3>You can't beat The Psychic!</h3>" +
    "<p>Wins: " + wins + "</p>" +
    "<p>Losses: " + losses + "</p>" +
    "<p>Guesses Left: " + numGuesses + "</p>" +
